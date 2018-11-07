4d9f1d168693e6d1eee4ca9bc49c70ef8e1193ae
realwrtoff <fmj_912@163.com>
2018-11-07 10:09:38 +0800

    add close

--- ai_router.go
+++ ai_router.go
@@ -1,28 +1,49 @@
 package ai_util
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 type AIRoute struct {
 	address string
 	engine  *gin.Engine
+	server  *http.Server
 }
 
 func (r *AIRoute) Init(address string) error {
 	r.address = address
-	r.engine = gin.Default()
+	r.engine = gin.New()
+	r.engine.Use(gin.Recovery())
 	return nil
 }
 
-func (r *AIRoute) SetGetHandler(path string, handlerFunc gin.HandlerFunc)  {
+func (r *AIRoute) SetGetHandler(path string, handlerFunc gin.HandlerFunc) {
 	r.engine.GET(path, handlerFunc)
 }
 
-func (r *AIRoute) SetPostHandler(path string, handlerFunc gin.HandlerFunc)  {
+func (r *AIRoute) SetPostHandler(path string, handlerFunc gin.HandlerFunc) {
 	r.engine.POST(path, handlerFunc)
 }
 
 func (r *AIRoute) Run() error {
-	return r.engine.Run(r.address)
+	// return r.engine.Run(r.address)
+	r.server = &http.Server{
+		Addr:    r.address,
+		Handler: r.engine,
+	}
+	if err := r.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
+
+// 优雅的退出
+func (r *AIRoute) Close() error {
+	// a timeout of 5 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return r.server.Shutdown(ctx)
 }
